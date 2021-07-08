b58fbda07069750379531764d4d9f6d43804c2e7
Ramandeep Malhotra <r.malhotra@adform.com>
2021-07-08 13:09:25 +0530

    added header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
 
 
 export default () => (
-    <div>
-        <h1>Hi There !!</h1>
-        <hr/>
-        <MarketingApp />
-    </div>
+    <BrowserRouter>
+        <div>
+            <Header />
+            <MarketingApp />
+        </div>
+    </BrowserRouter>
 );
