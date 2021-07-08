ef5d8d5042836daa8438cc2e3a05af6a93e215ef
Ramandeep Malhotra <r.malhotra@adform.com>
2021-07-08 16:46:05 +0530

    added randomised class names

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
 
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co',
+})
 
 export default () => (
-    <BrowserRouter>
-        <div>
-            <Header />
-            <MarketingApp />
-        </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+        <BrowserRouter>
+            <div>
+                <Header />
+                <MarketingApp />
+            </div>
+        </BrowserRouter>
+    </StylesProvider>
 );
